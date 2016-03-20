2728cfcce5e4d7c7ad34d156d6fe22fde12f2f18
Macbook <Macbook@Macbooks-MBP.lan>
2016-03-20 23:10:25 +0200

    parse logic updates

--- js/parse.js
+++ js/parse.js
@@ -1,6 +1,8 @@
 
 $(document).ready(function() {
 
+	var TEAMCOUNT = 5;
+
 	Parse.initialize("nu2ODMNFUme7MVibNXq0zhZNI3h8ACb7btaMmhJe", "hav8XU3gKQ7Gz3lLmW3dPvJuRnrT7xC3bmZhxKzW");
 	var TeamScores = Parse.Object.extend("TeamScores");
 
@@ -16,18 +18,25 @@ $(document).ready(function() {
 			teams.push(getDataFromRow(row));
 		});
 
-		Parse.Object.saveAll(teams, {
-			success: function(objs) {
-				console.log("Success");
-			},
-			error : function(error) {
-				console.log("Erorr");
-			}
-		});
+		// Parse.Object.saveAll(teams, {
+		// 	success: function(objs) {
+		// 		console.log("Success");
+		// 	},
+		// 	error : function(error) {
+		// 		console.log("Erorr");
+		// 	}
+		// });
 	}
 
 	function getDataFromRow (row) {
+		
 		console.log(row);
+
+		var name = getName(row);
+		var scores = getScores(row);
+		var total = $(row).find(".total_field").html();
+		
+		console.log(name + " - " + scores + " - " + total);
 
 		var team = new TeamScores();
 		team.set("scores", [1, 2, 3, 4, 0]);
@@ -36,10 +45,29 @@ $(document).ready(function() {
 		return team;
 	}
 
-	function handleResult(object) {
-		alert("Result. Object; " + object);
+	function getName (row) {
+		var name = $(row).filter(".data_team_name").html();
+		var td = $(row).find("td");
+
+		console.log(name + " - " + td);
 	}
 
+	function getScores(row) {
+
+		var fields = $(row).find("data_input_field");
+		var scores = [];
+
+		for (var i; i < fields.length; i++) {
+			var field = fields[i].html();
+			scores.push(field);
+		}
+
+		return scores;
+	}
+
+	function getTotal(row) {
+
+	}
 });
 
 

460613ee1a109be47cf54fff6399e4f29bf95fbd
Aare Undo <aare@wazombi.com>
2016-05-22 13:43:17 +0300

    refactor

--- calculator/js/main.js
+++ calculator/js/main.js
@@ -3,7 +3,6 @@
 $(document).ready(function() {
 
 	var counter = 0;
-	var roundCount = 5;
 
 	/****************** 
 		EVENTHANDLERS
@@ -51,7 +50,7 @@ $(document).ready(function() {
 		counter++;
 		header.show();
 
-		var row = getRow();
+		var row = getRow(counter);
 		$(".team_table tbody").append(row);
 		
 		if (add) {
@@ -197,76 +196,6 @@ $(document).ready(function() {
 
 		team.Name = inputFields[0].value;
 		SetCookies();
-	}
-
-	/****************** 
-		UI REFERENCES
-	*******************/
-
-	function getInputFieldsFromRow(row) {
-		return row.find(".data_input_field");
-	}
-
-	function getTotalFieldFromRow(row) {
-		return row.find(".total_field");
-	}
-
-	function getRow () {
-		var content = getRowContent();
-		return $("<tr class='team_row'></tr>").append(content);
-	}
-
-	function getRowContent() {
-		var content = "<td class='row_data data_number'> " + counter + "</td>";
-		content += getInputField("data_team_name");
-
-		for (var i = 0; i < roundCount; i++) {
-			content += getInputField("data_round_" + (i + 1))
-		}
-
-		content += getTotalLabel(0);
-
-		return content;
-	}
-
-	function getInputField(className) {
-		return "<td>" + "<input type='text' class='row_data data_input_field " + className + "'>" + "</td>";
-	}
-
-	function getTotalLabel(value) {
-		return "<td class='row_data data_number total_field'>" + value + "</td>";
-	}
-
-	function getHeader() {
-		return  "<th> </th> <th> Team </th> <th> I </th> <th> II </th> <th> III </th> <th> IV </th> <th> V </th>";
-	}
-
-	function getBody() {
-		return "<tbody> </tbody>";
-	}
-
-	function getPopupRow (index, team) {
-		var content = getPopupRowContent(index, team);
-		return $("<tr class='team_row team_row_popup'></tr>").append(content);
-	}
-
-	function getPopupRowContent (index, team) {
-		var content = "<td class='row_data data_number'> " + index + "</td>";
-		content += getPopupInputLabel(team.Name, "data_team_name");
-
-		for (var i = 0; i < roundCount; i++) {
-			var score = team.Scores[i];
-			content += getPopupInputLabel(score, "data_round_" + (i + 1))
-		}
-
-		content += getTotalLabel(team.Total());
-
-		return content;
-	}
-
-	function getPopupInputLabel(value, className) {
-		return "<td>" + "<input disabled='disabled' type='text' class='row_data data_input_field " + className + "'>" + "</td>";
-		// return "<td class='row_data data_input_field " + className + "'>" + value + "</td>";
 	}
 
 	/****************** 
---
+++ calculator/js/ui.js
@@ -0,0 +1,83 @@
+
+	/****************** 
+		UI REFERENCES
+	*******************/
+	var roundCount = 5;
+
+	function getInputFieldsFromRow(row) {
+		return row.find(".data_input_field");
+	}
+
+	function getTotalFieldFromRow(row) {
+		return row.find(".total_field");
+	}
+
+	function getRow (counter) {
+		var content = getRowContent(counter);
+		return $("<tr class='team_row'></tr>").append(content);
+	}
+
+	function getRowContent(counter) {
+		var content = "<td class='row_data data_number'> " + counter + "</td>";
+		content += getInputField("data_team_name");
+
+		for (var i = 0; i < roundCount; i++) {
+			content += getInputField("data_round_" + (i + 1))
+		}
+
+		content += getTotalLabel(0);
+
+		return content;
+	}
+
+	function getInputField(className) {
+		return "<td>" + "<input type='text' class='row_data data_input_field " + className + "'>" + "</td>";
+	}
+
+	function getTotalLabel(value) {
+		return "<td class='row_data data_number total_field'>" + value + "</td>";
+	}
+
+	function getHeader() {
+		return  "<th> </th> <th> Team </th> <th> I </th> <th> II </th> <th> III </th> <th> IV </th> <th> V </th>";
+	}
+
+	function getBody() {
+		return "<tbody> </tbody>";
+	}
+
+	function getPopupRow (index, team) {
+		var content = getPopupRowContent(index, team);
+		return $("<tr class='team_row team_row_popup'></tr>").append(content);
+	}
+
+	function getPopupRowContent (index, team) {
+		var content = "<td class='row_data data_number'> " + index + "</td>";
+		content += getPopupInputLabel(team.Name, "data_team_name");
+
+		for (var i = 0; i < roundCount; i++) {
+			var score = team.Scores[i];
+			content += getPopupInputLabel(score, "data_round_" + (i + 1))
+		}
+
+		content += getTotalLabel(team.Total());
+
+		return content;
+	}
+
+	function getPopupInputLabel(value, className) {
+		return "<td>" + "<input disabled='disabled' type='text' class='row_data data_input_field " + className + "'>" + "</td>";
+	}
+
+$(document).ready(function() {
+
+});
+
+
+
+
+
+
+
+
+
